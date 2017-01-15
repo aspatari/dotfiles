e0d5d6dcd128d35ba242402527e088ae03b4ac56
Artur Spatari <artemir0106@gmail.com>
2017-01-15 16:47:42 +0200

    Modify install sh

--- install.sh
+++ install.sh
@@ -2,13 +2,22 @@
 
 
 #Create symlink for .i3
-rm -rf ~/.i3
-ln -s ~/dotfiles/i3/ ~/.i3
-sudo ln -s ~/dotfiles/oblogout.conf /etc/oblogout.conf
+#rm -rf ~/.i3
+#ln -s ~/dotfiles/i3/ ~/.i3
+#sudo ln -s ~/dotfiles/oblogout.conf /etc/oblogout.conf
+
+
+#zsh
+##antigen
+curl https://cdn.rawgit.com/zsh-users/antigen/v1.3.2/bin/antigen.zsh > antigen.zsh
+sudo ln -s ~/dotfiles/zshrc ~/.zshrc
+
+yaourt -S zsh ruby-sass google-chrome pamac pamac-tray-appindicator git-kraken git antigen-git
 
 
 
-
+#fonts
+git clone https://github.com/powerline/fonts.git
 
 
 #vim
