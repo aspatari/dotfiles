7d7dc1c48cbe3d9bf55e82f1c627a430459b6d8b
Artur Spatari <artemir0106@gmail.com>
2017-02-23 15:14:13 +0200

    add some pkgs

--- install.sh
+++ install.sh
@@ -7,31 +7,49 @@
 #sudo ln -s ~/dotfiles/oblogout.conf /etc/oblogout.conf
 
 
+# Symlinks
+ln -s ~/dotfiles/zshrc ~/.zshrc
+
+
+
 #zsh
 ##antigen
 curl https://cdn.rawgit.com/zsh-users/antigen/v1.3.2/bin/antigen.zsh > antigen.zsh
-sudo ln -s ~/dotfiles/zshrc ~/.zshrc
+
 
 yaourt -S 
  zsh
  vim 
  ruby-sass 
- google-chrome 
- pamac 
- pamac-tray-appindicator 
+ google-chrome  
  gitkraken 
  git 
  antigen-git 
  jetbrains-toolbox
  java-environment-common
  qbittorrent-git
+ n1
+ valentina-studio 
+ franz-bin
+ visual-studio-code
+ guake-git 
+    
 
+
+
+
+
+ composer
+
+
+ plank
+ plank-config
+
+
+ pamac
+ pamac-tray-appindicator
 
 
 #fonts
 git clone https://github.com/powerline/fonts.git
 
-
-#vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
